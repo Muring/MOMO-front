5834195181d3b2d7848a723af594b33596351d67
MuRing <11esh@naver.com>
2024-04-09 10:33:14 +0900

    FIX: nuxt config developer tool disable

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss", "nuxt-swiper", "@pinia/nuxt"],
   plugins: [
     "@/plugins/global-functions.js", // 플러그인 파일의 경로
